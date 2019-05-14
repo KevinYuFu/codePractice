dca592a59c9de14bea39e73340909d19ed52cfe9
Kevin Yu Fu <Kevin.yu.fu@gmail.com>
2019-05-14 14:28:12 -0400

    Initial push and some python stuff

---
+++ pythonPractice/ex1.py
@@ -0,0 +1,11 @@
+'''
+Character Input
+'''
+
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+
+
+print("\nHey " + name + ", you will turn 100 in " + str(100 - age) + " years.")
+
+
---
+++ pythonPractice/ex2.py
@@ -0,0 +1,7 @@
+# Even Odd
+
+num = int(input("Give me a number: "))
+quality = "even" if num % 2 == 0 else "odd"
+
+print("\nThis number is " + quality)
+
